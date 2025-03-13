c75e0f8c4b2438de4843650aaa7e52f6b65268fb
reypjbaliguat <reypjbaliguat@gmail.com>
2025-03-13 15:00:46 +0800

    fix(footer): update padding y

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="h-lg:mt-0 h-2xl:mt-36 bg-deep-teal text-lime-green w-full py-12 grid grid-cols-1 px-8">
+    <footer className="h-lg:mt-0 h-2xl:mt-36 bg-deep-teal text-lime-green w-full py-14 grid grid-cols-1 px-8">
       <div className="flex justify-between w-full items-center leading-none">
         <span className="text-4xl"> Rey PJ Baliguat</span>
         <span className="text-logo"> sesimi</span>
